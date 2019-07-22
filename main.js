d3617541895f337d0bf8275e123af34745432841
ralph1786 <ralph1786@gmail.com>
2019-07-22 15:20:05 -0400

    created UI and Song classes

---
+++ main.js
@@ -0,0 +1,63 @@
+//Song Class: Represents a Song
+class Song {
+  constructor(title, author, genre) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+  }
+}
+
+//UI Class: Handles UI tasks
+class UI {
+  static displaySongs() {
+    const listSongs = [
+      {
+        title: "Play Rock",
+        author: "Someone",
+        genre: "Rock"
+      },
+      {
+        title: "Play Hip-Hop",
+        author: "Someone Else",
+        genre: "Hip-Hop"
+      }
+    ];
+    const songs = listSongs;
+
+    songs.forEach(song => UI.addSongToList(song));
+  }
+
+  static addSongToList(song) {
+    const list = document.querySelector("#song-list");
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+        <td>${song.title}</td>
+        <td>${song.author}</td>
+        <td>${song.genre}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+
+    list.appendChild(row);
+  }
+}
+
+//Store Class: Handles Local Storage
+
+//Event: Show Songs
+document.addEventListener("DOMContentLoaded", UI.displaySongs);
+
+//Event: Add Song
+document.querySelector("#song-form").addEventListener("submit", e => {
+  e.preventDefault();
+  //get form values
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const genre = document.querySelector("#genre").value;
+
+  //instantiate song
+  const song = new Song(title, author, genre);
+  console.log(song);
+});
+
+//Event: Delete Song
